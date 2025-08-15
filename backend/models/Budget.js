6f7b7b1b78366cec2754eafcc6477a232b982b2e
TacNap <149756694+TacNap@users.noreply.github.com>
2025-08-15 18:57:52 +1000

    BTA-16 added routes to server.js. Fixed an issue with Budget model

--- backend/models/Budget.js
+++ backend/models/Budget.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const budgetSchema = new mongoose.Schema({
-    userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     category: { type: String, enum: ['Groceries', 'Rent', 'Entertainment', 'Fuel', 'Bills', 'Other'], required: true },
     amount: { type: Number, required: true }
 });
